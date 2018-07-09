7ece0185e7126ff80b9b5eebfad0142325d545ea
Ben Kimock <kimockb@gmail.com>
2018-07-09 14:25:03 -0400

    rename

--- run_the_pipeline.py
+++ run_the_pipeline.py
@@ -3,11 +3,12 @@ import argparse
 
 import h5py
 import numpy as np
-import caesar
 import yt
 import octree
 import bpass
 import lycrt
+import numexpr
+
 
 parser = argparse.ArgumentParser(description="THE CLASP")
 parser.add_argument('snapshot')
@@ -20,58 +21,58 @@ snap = args.snapshot
 caesar_path = args.caesar or os.path.join(os.path.dirname(snap), f'caesar_{os.path.basename(snap)}')
 output_dir = args.outdir or '.'
 
+
+caesar_files = sorted([f.path for f in os.scandir(os.path.dirname(caesar_path)) if 'caesar_00' in f.name])
+
+galaxy_pos = h5py.File(caesar_files[0])['galaxy_data']['pos'][1]
+
+for f in caesar_files:
+    pos = h5py.File(f)['galaxy_data']['pos'][:20]
+    distance = np.sum((pos - galaxy_pos)**2, axis=1)
+    galaxy_pos = pos[np.argmin(distance)]
+    print(galaxy_pos)
+
+    if f == caesar_path:
+        break
+
+
 ds = yt.load(snap)
-data = ds.all_data()
+SIZE = ds.quan(250.0, 'kpccm')
 
-"""
-Use CAESAR to extract the high-resolution halo 
-"""
+galaxy_pos = yt.YTArray(galaxy_pos, 'kpccm', ds.unit_registry)
 
-# Cache the caesar file next to the original- should probably put them local
-try:
-    obj = caesar.load(caesar_path)
-except:
-    obj = caesar.CAESAR(ds)
-    obj.member_search(unbind_halos=True, unbind_galaxies=True)
-    try:
-        obj.save(caesar_path)
-    except Exception as e:
-        print("Could not save CAESAR file, {}".format(e))
+f = h5py.File(args.snapshot)
 
+gas_position = yt.YTArray(f['PartType0/Coordinates'][:], 'code_length', ds.unit_registry).convert_to_units('kpccm')
 
-SIZE = ds.quan(250.0, 'kpccm').convert_to_units('code_length')
-
-# Select based on the virial radius of the central galaxy
-galaxy = obj.halos[0].galaxies[0]
-galaxy_pos = galaxy.pos.convert_to_units('code_length')
-
-gas_position = data[('PartType0', 'Coordinates')]
-star_position = data[('PartType4', 'Coordinates')]
-
-glist = ((gas_position - galaxy_pos)**2).sum(axis=1) < (2 * SIZE)**2
-slist = ((star_position - galaxy_pos)**2).sum(axis=1) < (2 * SIZE)**2
+lhs = numexpr.evaluate('sum((gas_position - galaxy_pos)**2, axis=1)')
+glist = numexpr.evaluate('lhs < (2 * SIZE)**2')
+assert np.sum(glist) > 0
 
 # Filter the gas particles
-gas_position = data[('PartType0', 'Coordinates')][glist]
-mass = data[('PartType0', 'Masses')][glist]
-velocity = data[('PartType0', 'Velocities')][glist]
-internal_energy = data[('PartType0', 'InternalEnergy')][glist]
-neutral_fraction = data[('PartType0', 'NeutralHydrogenAbundance')][glist]
-electron_fraction = data[('PartType0', 'ElectronAbundance')][glist]
-gas_metallicity = data[('PartType0', 'Metallicity_00')][glist]
-smoothing_length = data[('PartType0', 'SmoothingLength')][glist]
+gas_position = gas_position[glist]
+velocity = f['PartType0/Velocities'][:][glist]
+mass = f['PartType0/Masses'][glist]
+internal_energy = f['PartType0/InternalEnergy'][glist]
+neutral_fraction = f['PartType0/NeutralHydrogenAbundance'][glist]
+electron_fraction = f['PartType0/ElectronAbundance'][glist]
+smoothing_length = f['PartType0/SmoothingLength'][glist]
+gas_metallicity = f['PartType0/Metallicity'][:, 0][glist]
 
 # Filter the star particles
-star_metallicity = data[('PartType4', 'Metallicity_00')][slist]
-star_position = data[('PartType4', 'Coordinates')][slist]
+star_position = yt.YTArray(f['PartType4/Coordinates'][:], 'code_length', ds.unit_registry).convert_to_units('kpccm')
+lhs = numexpr.evaluate('sum((star_position - galaxy_pos)**2, axis=1)')
+slist = numexpr.evaluate('lhs < (2 * SIZE)**2')
 
-# Formation time is given in redshift at formation, convert to Gyr
-scalefactor = data[('PartType4', 'StellarFormationTime')][slist]
+star_position = star_position[slist]
+star_metallicity = f['PartType4/Metallicity'][:, 0][slist]
+
+scalefactor = f['PartType4/StellarFormationTime'][slist]
 formation_z = (1. / scalefactor) - 1.
 yt_cosmo = yt.utilities.cosmology.Cosmology()
 stellar_formation_age = yt_cosmo.t_from_z(formation_z).in_units('Gyr')
 # Age of the universe right now
-simtime = yt_cosmo.t_from_z(obj.simulation.redshift).in_units('Gyr')
+simtime = yt_cosmo.t_from_z(ds.current_redshift).in_units('Gyr')
 stellar_ages = (simtime - stellar_formation_age).in_units('Gyr')
 
 log_luminosity = bpass.compute_stellar_luminosity(stellar_ages, star_metallicity)
@@ -136,9 +137,9 @@ n_phi           1""".format(octreefile.name, starfile.name))
     Mpc = 1e6 * pc              # 1 Mpc = 10^6 pc
     Msun = 1.988435e33          # Solar mass in g
 
-   # write to a hdf5 file
+    # write to a hdf5 file
     f = h5py.File(os.path.join(output_dir, f'converted_{os.path.basename(snap)}'), 'w')
-    f.attrs['redshift'] = np.float64(obj.simulation.redshift)
+    f.attrs['redshift'] = np.float64(ds.current_redshift)
     f.attrs['n_cells'] = np.int32(tree.TOTAL_NUMBER_OF_CELLS)
     f.create_dataset('parent', data=np.array(tree.parent_ID, dtype=np.int32))
     f.create_dataset(
@@ -188,6 +189,4 @@ n_phi           1""".format(octreefile.name, starfile.name))
             dtype=np.float64))  # Cell velocities (cm/s)
     f['v'].attrs['units'] = b'cm/s'
     f.close()
-
-# Now we can finally kick off COLT
 
