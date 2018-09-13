129decf9da2d1c17b996d42d9ea9d4200ba26f14
Ben Kimock <kimockb@gmail.com>
2018-09-13 14:36:57 -0400

    black on white and conclusion

--- slides.py
+++ slides.py
@@ -1,8 +1,13 @@
 import matplotlib
+matplotlib.rcParams['savefig.facecolor'] = 'k'
+matplotlib.rcParams['text.color'] = 'w'
+matplotlib.rcParams['axes.edgecolor'] = 'w'
+matplotlib.rcParams['axes.labelcolor'] = 'w'
+matplotlib.rcParams['xtick.color'] = 'w'
+matplotlib.rcParams['ytick.color'] = 'w'
 
 matplotlib.use("Agg")
 matplotlib.rc("font", family="STIXGeneral")
-# matplotlib.rc('text', usetex=True)
 matplotlib.rcParams.update({"font.size": 22})
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
@@ -15,6 +20,7 @@ from scipy.misc import imread
 
 
 style_name = {"mathtext.fontset": "cm", "mathtext.fallback_to_cm": True}
+cmap = 'magma'
 
 
 with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
@@ -76,6 +82,27 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
     text.remove()
     ax.remove()
 
+    # Bullet-ish slide with figure
+    fig.suptitle("Modern View of LAB-1", fontsize=52)
+    ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
+    ax.axis("off")
+    text = fig.text(
+        0.05,
+        0.5,
+        "Ly$\\alpha$ ~ $10^{44}$ erg/s\nR ~ 100 kpc\nz ~ 2-5\n\nStar formation or AGN\nLittle/No radio emission\nOverdense regions",
+        va="center",
+        ha="left",
+        fontsize=48,
+        wrap=True,
+    )
+    ax2 = fig.add_axes([0.6, bottom, 0.4, 1.0-bottom-top])
+    ax2.imshow(imread('1920px-Lyman_Alpha_Blob.jpg'))
+    ax2.axis("off")
+    pdf.savefig(fig, dpi=100)
+    ax2.remove()
+    text.remove()
+    ax.remove()
+
     # Bullet-ish slide
     fig.suptitle("Lyman-alpha", fontsize=52)
     ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
@@ -92,10 +119,34 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
     pdf.savefig(fig, dpi=100)
     text.remove()
     ax.remove()
-
+    
     ds = yt.load('/ufrc/narayanan/desika.narayanan/gizmo_runs/MassiveFIRE/FIRE2/A2_res33000/snapdir_120/snapshot_120.0.hdf5')
     obj = caesar.load('/ufrc/narayanan/kimockb/fire_caesar/caesar_snapshot_120.hdf5')
-    
+    gal = obj.galaxies[0]
+    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/120/tot_SMC.h5")
+
+    # Bullet-ish slide
+    ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
+    ax.axis("off")
+    fig.suptitle('')
+    text = fig.text(
+        0.05,
+        0.5,
+        f"""z = {ds.current_redshift:.2f}
+SFR = {gal.sfr:.2e} {gal.sfr.units}
+Mtot = {gal.masses['total']:.2e} {gal.masses['total'].units}
+Mstar = {gal.masses['stellar']:.2e} {gal.masses['stellar'].units}
+fgas = {gal.gas_fraction:.3f}
+""",
+        va="center",
+        ha="left",
+        fontsize=48,
+        wrap=True,
+    )
+    pdf.savefig(fig, dpi=100)
+    text.remove()
+    ax.remove()
+
     # Plot our ProjectionPlots with their own axes objects
     for axis in ['x', 'y', 'z']:
         plot = yt.ProjectionPlot(
@@ -105,9 +156,10 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
             width=(250, "kpc"),
             center=obj.galaxies[0].pos.in_units("kpc"),
         )
+        plot.set_zlim(('gas', 'density'), 1e-4, 1e2)
         plot.set_font_size(22)
-        plot.set_cmap(("gas", "density"), cmap="inferno") # TODO: Why doesn't this do anything
         density_plot = plot.plots[("gas", "density")]
+        density_plot.image.set_cmap(cmap)
         density_plot.figure.set_size_inches(12.80, 10.24)
         density_plot.axes.set_position([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         density_plot.figure.suptitle("Is this an LAB?", fontsize=52)
@@ -117,36 +169,58 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
         pdf.savefig(density_plot.figure, dpi=100)
 
     # Figure slide
-    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/120/tot_SMC.h5")
     colt_images = colt_file["LOS/SB"][:4]
     for image in colt_images:
         image = image.clip(1e-23, 1e-10)
 
-        fig.suptitle(f"Is this an LAB? (z={colt_file.attrs['z']:.2f})", fontsize=52)
+        fig.suptitle(f"Is this an LAB?", fontsize=52)
 
         ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         plot_image = ax.imshow(
             np.flipud(image),
-            cmap="inferno",
+            cmap=cmap,
             norm=LogNorm(vmin=1e-23, vmax=1e-10, clip=True),
-            extent=[-250, 250, -250, 250],
+            extent=[-125, 125, -125, 125],
         )
-        contour = ax.contour(image, levels=[1.4e-18], colors='w', extent=[-250, 250, -250, 250], linewidths=[0.5])
+        contour = ax.contour(image, levels=[1.4e-18], colors='w', extent=[-125, 125, -125, 125], linewidths=[0.5])
         cbar = fig.colorbar(plot_image, pad=0)
 
-        ax.set_xlabel("(kpc)")
-        ax.set_ylabel("(kpc)")
+        ax.set_xlabel("(kpc)", color='w')
+        ax.set_ylabel("(kpc)", color='w')
         cbar.set_label(
             "Ly$\\alpha$ Surface Brightness $\left(\\frac{\mathrm{erg}}{\mathrm{s}\,\mathrm{cm}^2\,\mathrm{arcsec}^2}\\right)$"
         )
-
+        
         pdf.savefig(fig, dpi=100)
         cbar.remove()
         ax.remove()
 
     ds = yt.load('/ufrc/narayanan/desika.narayanan/gizmo_runs/MassiveFIRE/FIRE2/A2_res33000/snapdir_172/snapshot_172.0.hdf5')
     obj = caesar.load('/ufrc/narayanan/kimockb/fire_caesar/caesar_snapshot_172.hdf5')
+    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/172/tot_SMC.h5")
     
+    # Bullet-ish slide
+    ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
+    ax.axis("off")
+    fig.suptitle('')
+    text = fig.text(
+        0.05,
+        0.5,
+        f"""z = {ds.current_redshift:.2f}
+SFR = {gal.sfr:.2e} {gal.sfr.units}
+Mtot = {gal.masses['total']:.2e} {gal.masses['total'].units}
+Mstar = {gal.masses['stellar']:.2e} {gal.masses['stellar'].units}
+fgas = {gal.gas_fraction:.3f}
+""",
+        va="center",
+        ha="left",
+        fontsize=48,
+        wrap=True,
+    )
+    pdf.savefig(fig, dpi=100)
+    text.remove()
+    ax.remove()
+
     # Plot our ProjectionPlots with their own axes objects
     for axis in ['x', 'y', 'z']:
         plot = yt.ProjectionPlot(
@@ -156,9 +230,10 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
             width=(250, "kpc"),
             center=obj.galaxies[0].pos.in_units("kpc"),
         )
+        plot.set_zlim(('gas', 'density'), 1e-4, 1e2)
         plot.set_font_size(22)
-        plot.set_cmap(("gas", "density"), cmap="inferno") # TODO: Why doesn't this do anything
         density_plot = plot.plots[("gas", "density")]
+        density_plot.image.set_cmap(cmap)
         density_plot.figure.set_size_inches(12.80, 10.24)
         density_plot.axes.set_position([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         density_plot.figure.suptitle("Is this an LAB?", fontsize=52)
@@ -167,18 +242,18 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
         density_plot.cax.set_position([0.815, 0.09, (0.9-0.09)/25, 0.9-0.09])
         pdf.savefig(density_plot.figure, dpi=100)
 
+
     # Figure slide
-    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/172/tot_SMC.h5")
     colt_images = colt_file["LOS/SB"][:4]
     for image in colt_images:
         image = image.clip(1e-23, 1e-10)
 
-        fig.suptitle(f"Is this an LAB? (z={colt_file.attrs['z']:.2f})", fontsize=52)
+        fig.suptitle(f"Is this an LAB?", fontsize=52)
 
         ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         plot_image = ax.imshow(
             np.flipud(image),
-            cmap="inferno",
+            cmap=cmap,
             norm=LogNorm(vmin=1e-23, vmax=1e-10, clip=True),
             extent=[-250, 250, -250, 250],
         )
@@ -197,6 +272,30 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
 
     ds = yt.load('/ufrc/narayanan/desika.narayanan/gizmo_runs/MassiveFIRE/FIRE2/A2_res33000/snapdir_214/snapshot_214.0.hdf5')
     obj = caesar.load('/ufrc/narayanan/kimockb/fire_caesar/caesar_snapshot_214.hdf5')
+    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/214/tot_SMC.h5")
+
+    # Bullet-ish slide
+    ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
+    ax.axis("off")
+    fig.suptitle('')
+    text = fig.text(
+        0.05,
+        0.5,
+        f"""z = {ds.current_redshift:.2f}
+SFR = {gal.sfr:.2e} {gal.sfr.units}
+Mtot = {gal.masses['total']:.2e} {gal.masses['total'].units}
+Mstar = {gal.masses['stellar']:.2e} {gal.masses['stellar'].units}
+fgas = {gal.gas_fraction:.3f}
+""",
+        va="center",
+        ha="left",
+        fontsize=48,
+        wrap=True,
+    )
+    pdf.savefig(fig, dpi=100)
+    text.remove()
+    ax.remove()
+
     
     # Plot our ProjectionPlots with their own axes objects
     for axis in ['x', 'y', 'z']:
@@ -207,9 +306,10 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
             width=(250, "kpc"),
             center=obj.galaxies[0].pos.in_units("kpc"),
         )
+        plot.set_zlim(('gas', 'density'), 1e-4, 1e2)
         plot.set_font_size(22)
-        plot.set_cmap(("gas", "density"), cmap="inferno") # TODO: Why doesn't this do anything
         density_plot = plot.plots[("gas", "density")]
+        density_plot.image.set_cmap(cmap)
         density_plot.figure.set_size_inches(12.80, 10.24)
         density_plot.axes.set_position([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         density_plot.figure.suptitle("Is this an LAB?", fontsize=52)
@@ -219,17 +319,16 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
         pdf.savefig(density_plot.figure, dpi=100)
 
     # Figure slide
-    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/214/tot_SMC.h5")
     colt_images = colt_file["LOS/SB"][:4]
     for image in colt_images:
         image = image.clip(1e-23, 1e-10)
 
-        fig.suptitle(f"Is this an LAB? (z={colt_file.attrs['z']:.2f})", fontsize=52)
+        fig.suptitle(f"Is this an LAB?", fontsize=52)
 
         ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         plot_image = ax.imshow(
             np.flipud(image),
-            cmap="inferno",
+            cmap=cmap,
             norm=LogNorm(vmin=1e-23, vmax=1e-10, clip=True),
             extent=[-250, 250, -250, 250],
         )
@@ -248,6 +347,30 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
 
     ds = yt.load('/ufrc/narayanan/desika.narayanan/gizmo_runs/MassiveFIRE/FIRE2/A2_res33000/snapdir_277/snapshot_277.0.hdf5')
     obj = caesar.load('/ufrc/narayanan/kimockb/fire_caesar/caesar_snapshot_277.hdf5')
+    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/277/tot_SMC.h5")
+
+    # Bullet-ish slide
+    ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
+    ax.axis("off")
+    fig.suptitle('')
+    text = fig.text(
+        0.05,
+        0.5,
+        f"""z = {ds.current_redshift:.2f}
+SFR = {gal.sfr:.2e} {gal.sfr.units}
+Mtot = {gal.masses['total']:.2e} {gal.masses['total'].units}
+Mstar = {gal.masses['stellar']:.2e} {gal.masses['stellar'].units}
+fgas = {gal.gas_fraction:.3f}
+""",
+        va="center",
+        ha="left",
+        fontsize=48,
+        wrap=True,
+    )
+    pdf.savefig(fig, dpi=100)
+    text.remove()
+    ax.remove()
+
     
     # Plot our ProjectionPlots with their own axes objects
     for axis in ['x', 'y', 'z']:
@@ -258,9 +381,10 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
             width=(250, "kpc"),
             center=obj.galaxies[0].pos.in_units("kpc"),
         )
+        plot.set_zlim(('gas', 'density'), 1e-4, 1e2)
         plot.set_font_size(22)
-        plot.set_cmap(("gas", "density"), cmap="inferno") # TODO: Why doesn't this do anything
         density_plot = plot.plots[("gas", "density")]
+        density_plot.image.set_cmap(cmap)
         density_plot.figure.set_size_inches(12.80, 10.24)
         density_plot.axes.set_position([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         density_plot.figure.suptitle("Is this an LAB?", fontsize=52)
@@ -270,17 +394,16 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
         pdf.savefig(density_plot.figure, dpi=100)
 
     # Figure slide
-    colt_file =  h5py.File("/ufrc/narayanan/kimockb/fireA2/277/tot_SMC.h5")
     colt_images = colt_file["LOS/SB"][-4:]
     for image in colt_images:
         image = image.clip(1e-23, 1e-10)
 
-        fig.suptitle(f"Is this an LAB? (z={colt_file.attrs['z']:.2f})", fontsize=52)
+        fig.suptitle(f"Is this an LAB?", fontsize=52)
 
         ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
         plot_image = ax.imshow(
             np.flipud(image),
-            cmap="inferno",
+            cmap=cmap,
             norm=LogNorm(vmin=1e-23, vmax=1e-10, clip=True),
             extent=[-250, 250, -250, 250],
         )
@@ -296,4 +419,31 @@ with PdfPages("symposium.pdf") as pdf, matplotlib.style.context(
         pdf.savefig(fig, dpi=100)
         cbar.remove()
         ax.remove()
+
+
+    # Bullet-ish slide
+    ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
+    ax.axis("off")
+    fig.suptitle('Conclusions?', fontsize=52)
+    text = fig.text(
+        0.05,
+        0.5,
+        f"""LABs are strange
+
+Do the experiment!
+
+Collaboration is very valuable
+
+Special thanks to Xiangcheng Ma and Aaron Smith for their code and expertise
+""",
+        va="center",
+        ha="left",
+        fontsize=48,
+        wrap=True,
+    )
+    pdf.savefig(fig, dpi=100)
+    text.remove()
+    ax.remove()
+
+
 
