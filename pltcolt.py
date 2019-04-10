think I fixed my units mistakes, finally

pltcolt.py
@@ -1,21 +1,24 @@
-import glob
 import argparse
 import numpy as np
+import os
 import matplotlib
-
-matplotlib.use("Agg")
-from matplotlib import pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
+import yt
 import h5py
+from matplotlib import pyplot as plt
+
+os.environ["OMP_NUM_THREADS"] = "1"
 
 parser = argparse.ArgumentParser("COLT Plotter")
-parser.add_argument("file_patterns", nargs="+")
+parser.add_argument("ds")
+parser.add_argument("colt_file")
 parser.add_argument("-o", "--output")
-parser.add_argument("--dpi", default=96*8, type=int)
-parser.add_argument("--uv", action='store_true', default=False)
+parser.add_argument("--dpi", default=96, type=int)
+parser.add_argument("--uv", action="store_true", default=False)
+parser.add_argument("--muse", action="store_true", default=False)
+parser.add_argument("--sb_image", default=0, type=int)
 args = parser.parse_args()
 
-'''
+"""
 matplotlib.rcParams.update(
     {
         "savefig.facecolor": "k",
@@ -30,7 +33,7 @@ matplotlib.rcParams.update(
         "mathtext.fallback_to_cm": True,
     }
 )
-'''
+"""
 
 matplotlib.rcParams.update(
     {
@@ -47,71 +50,109 @@ matplotlib.rcParams.update(
     }
 )
 
-#max_clip = 1e-4
-#min_clip = 1e-9
-max_clip = 1e-16
-min_clip = 1e-22
+if args.uv:
+    max_clip = 1e-4
+    min_clip = 1e-9
+else:
+    max_clip = 1e-16
+    min_clip = 1e-22
 
 bottom = 0.07
 top = 0.02
 left = 0.08
 right = 0.01
 
-with PdfPages(args.output) as pdf:
-    for pattern in args.file_patterns:
-        for output_path in sorted(glob.glob(pattern)):
+ds = yt.load(args.ds)
 
-            with h5py.File(output_path, "r") as f:
-                if args.uv:
-                    image = f["LOS/SB_UV_LOS"][0]
-                else:
-                    image = f["LOS/SB"][0]
-                redshift = f.attrs["z"]
-                # COLT units are in cm, convert to kpc
-                image_radius = f["LOS"].attrs["SB_radius"] / 3.085677581467192e21
-                lum = f.attrs["L_Lya"] * np.sum(f['esc/weight'])
+with h5py.File(args.colt_file, "r") as f:
+    image = f["LOS/SB"][args.sb_image]
+    redshift = f.attrs["z"]
+    # COLT units are in cm, convert to kpc
+    lum = f.attrs["L_Lya"] * np.sum(f["esc/weight"])
+    print('luminosity', lum)
+    image_radius = ds.quan(250.0, 'kpccm')
+    #image_radius = ds.quan(f["LOS"].attrs["SB_radius"], "cm").to("kpccm")
+    colt_pixel_size = np.sqrt(f["LOS"].attrs["SB_arcsec2"])
 
-            #fig = plt.figure(figsize=(12., 10.))
-            fig = plt.figure(figsize=(6.0, 5.0))
-            print(image.max())
 
-            image = image.clip(min_clip, max_clip)
-            #fig.suptitle(f"L_Lya = {lum:.2e}", fontsize=36, color='w')
-            ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
-            ax.text(0.5, 0.99, f"z = {redshift:.2f}\nL_Lya = {lum:.2e}", horizontalalignment='center', verticalalignment='top', transform=ax.transAxes, color='w')
-            plot_image = ax.imshow(
-                image,
-                cmap="viridis",
-                norm=matplotlib.colors.LogNorm(vmin=min_clip, vmax=max_clip),
-                extent=[
-                    -image_radius,
-                    image_radius,
-                    -image_radius,
-                    image_radius,
-                ],
-                resample=True,
-                interpolation="nearest",
-            )
-            cbar = fig.colorbar(plot_image, pad=0)
-            cbar.ax.tick_params(axis="both", direction="in", which="both")
+#image = image.clip(min_clip, max_clip)
 
-            ax.set_xlabel("(kpc)")
-            ax.set_ylabel("(kpc)")
-            cbar.set_label(
-                "Ly$\\alpha$ Surface Brightness $\left(\\frac{\mathrm{erg}}{\mathrm{s}\,\mathrm{cm}^2\,\mathrm{arcsec}^2}\\right)$"
-            )
-            ax.tick_params(
-                axis="both",
-                direction="in",
-                which="both",
-                bottom=True,
-                top=True,
-                left=True,
-                right=True,
-            )
-            ax.xaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(10))
-            ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(10))
+fig = plt.figure(figsize=(6.0, 5.0))
+ax = fig.add_axes([left, bottom, 1.0 - left - right, 1.0 - bottom - top])
+ax.text(
+    0.5,
+    0.99,
+    f"z = {redshift:.2f}\nL_Lya = {lum:.2e}",
+    horizontalalignment="center",
+    verticalalignment="top",
+    transform=ax.transAxes,
+    color="w",
+)
 
-            fig.set_size_inches(12.80, 10.24)
-            pdf.savefig(fig, dpi=args.dpi, bbox_inches='tight')
-            plt.close(fig)
+if args.muse:
+    from scipy.ndimage import zoom
+    from scipy.ndimage.filters import gaussian_filter
+
+    muse_pixel_size = 0.2 # arcseconds/pixel
+    muse_psf_fwhm = 0.6
+
+    # Apply MUSE resolution via gaussian convolution
+    sigma = muse_psf_fwhm / colt_pixel_size / 2.355 # convert from FWHM to sigma
+    muse_image = gaussian_filter(image, sigma)
+   
+    # Bin to MUSE pixel scale
+    muse_image = zoom(muse_image, colt_pixel_size / muse_pixel_size)
+
+    print('colt', np.sum(image) * colt_pixel_size**2)
+    print('muse', np.sum(muse_image) * muse_pixel_size**2)
+
+    image_radius /= ds.cosmology.angular_scale(0.0, ds.current_redshift).to("kpccm/arcsec")
+
+    plot_image = ax.imshow(
+        muse_image,
+        cmap="viridis",
+        norm=matplotlib.colors.LogNorm(vmin=min_clip, vmax=max_clip),
+        extent=[-image_radius, image_radius, -image_radius, image_radius],
+        resample=False,
+        interpolation="nearest",
+    )
+    ax.set_xlabel("(arcsec)")
+    ax.set_ylabel("(arcsec)")
+
+else:
+    plot_image = ax.imshow(
+        image,
+        cmap="viridis",
+        norm=matplotlib.colors.LogNorm(vmin=min_clip, vmax=max_clip),
+        extent=[-image_radius, image_radius, -image_radius, image_radius],
+        resample=False,
+        interpolation="nearest",
+    )
+    ax.set_xlabel("(comoving kpc)")
+    ax.set_ylabel("(comoving kpc)")
+
+cbar = fig.colorbar(plot_image, pad=0)
+cbar.ax.tick_params(axis="both", direction="in", which="both")
+
+if args.uv:
+    cbar.set_label(
+        r"Lyman-Continuum Surface Brightness $\left(\frac{\mathrm{erg}}{\mathrm{s}\,\mathrm{cm}^2\,\mathrm{arcsec}^2}\right)$"
+    )
+else:
+    cbar.set_label(
+        r"Ly$\alpha$ Surface Brightness $\left(\frac{\mathrm{erg}}{\mathrm{s}\,\mathrm{cm}^2\,\mathrm{arcsec}^2}\right)$"
+    )
+ax.tick_params(
+    axis="both",
+    direction="in",
+    which="both",
+    bottom=True,
+    top=True,
+    left=True,
+    right=True,
+)
+ax.xaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(10))
+ax.yaxis.set_minor_locator(matplotlib.ticker.MultipleLocator(10))
+
+fig.set_size_inches(12.80, 10.24)
+fig.savefig(args.output, dpi=args.dpi, bbox_inches="tight")